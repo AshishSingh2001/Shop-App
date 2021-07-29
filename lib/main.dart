ddfc3c006f4d2c9d215bb9abcb6ad8fe58b1e81e
Ashish Singh <ashish.k.singh1001@gmail.com>
2021-07-29 15:42:41 +0530

    added cart provider and mutiple provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/models/products.dart';
+import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 
@@ -9,8 +10,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(), 
+    return MultiProvider(
+      providers: [
+        Provider<Products>(
+          create: (_) => Products(),
+        ),
+        Provider<Cart>(
+          create: (_) => Cart(),
+        )
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
